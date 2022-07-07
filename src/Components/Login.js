7861a1a72e32159a709ebacb2e00b885594b9fc6
Agustin Ricetta <r.agustinricetta@gmail.com>
2022-07-06 23:28:57 -0300

    registro y roto

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Navbar from './Navbar'
+import './Login.css'
+import { Link } from 'react-router-dom'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
+
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMsg, setErrorMsg] = useState ("");
+  const [successMsg, setSuccessMsg] = useState("");
+  const auth=getAuth()  
+  const navigate = useNavigate()
+// SE ROMPE ACA
   return (
     <div>
        <Navbar/>
