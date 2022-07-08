2424df51e237b1b9fc68150747036dc8ba326aa3
Agustin Ricetta <r.agustinricetta@gmail.com>
2022-07-08 00:28:59 -0300

    erro

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,26 +8,26 @@ import { collection, doc, getDocs, query, where } from 'firebase/firestore'
 
 const Home = () => {
   function GetCurrentUser(){
-    const [user, setUser ] = useState('')
-    const usersCollectionRef = collection(db, "users")
-    useEffect(()=>{
-        auth.onAuthStateChanged(userlogged=>{
-          if(userlogged){
-            const getUsers = async () => {
-              const q =query(collection(db,"users"), where("uuid","==",userlogged.uid))
-              // console.log(q)
-              const data = await getDocs(q)
-              setUser(data.docs.map((doc)=>({...doc.data(),id:doc.id})))
+      const [user, setUser ] = useState('')
+      const usersCollectionRef = collection(db, "users")
+      useEffect(()=>{
+          auth.onAuthStateChanged(userlogged=>{
+            if(userlogged){
+              const getUsers = async () => {
+                const q = query(collection(db,"users"), where("uid","==",userlogged.uid))
+                // console.log(q)
+                const data = await getDocs(q)
+                setUser(data.docs.map((doc)=>({...doc.data(),id:doc.id})))
+              }
+              getUsers();
             }
-            getUsers();
-          }
-          else{
-            setUser(null)
-          }
-        })
-      },[ ])
-      return user
-    }
+            else{
+              setUser(null)
+            }
+          })
+        },[ ])
+        return user
+      }
 
   
   const loggeduser = GetCurrentUser ();
